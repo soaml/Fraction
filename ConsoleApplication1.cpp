9a705542a8c9a023244a8d6ef65a6b40665d0f94
soaml <95581755+soaml@users.noreply.github.com>
2021-12-05 19:16:44 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include "fraction.h"
+using namespace std;
+int main()
+{
+	Fraction fraction1;
+	Fraction fraction2;
+	cout << "enter first fraction: ";
+	cin >> fraction1;
+	cout << "enter second fraction: ";
+	cin >> fraction1;
+	long n;
+	cout << "enter factor number: ";
+	cin >> n;
+
+	try {
+		fraction1.check();
+	}
+	catch (...) {
+		cout << "error";
+		return 1;
+	}
+
+	try {
+		fraction2.check();
+	}
+	catch (...) {
+		cout << "error";
+		return 1;
+	}
+	cout << fraction1 << " + " << fraction2 << " = ";
+	(fraction1 + fraction2).print();
+	cout << endl;
+
+	cout << fraction1 << " - " << fraction2 << " = ";
+	(fraction1 - fraction2).print();
+	cout << endl;
+
+	cout << fraction1 << " * " << fraction2 << " = ";
+	(fraction1 * fraction2).print();
+	cout << endl;
+
+	cout << fraction1 << " / " << fraction2 << " = ";
+	(fraction1 / fraction2).print();
+	cout << endl;
+
+	cout << " + " << fraction1 << " = ";
+	(+fraction1).print();
+	cout << endl;
+
+	cout << " - " << fraction1 << " = ";
+	(-fraction1).print();
+	cout << endl;
+
+	cout << fraction1 << " + " << n << " = ";
+	(fraction1 + n).print();
+	cout << endl;
+
+	cout << fraction1 << " - " << n << " = ";
+	(fraction1 - n).print();
+	cout << endl;
+
+	cout << fraction1 << " * " << n << " = ";
+	(fraction1 * n).print();
+	cout << endl;
+
+	cout << fraction1 << " / " << n << " = ";
+	(fraction1 / n).print();
+	cout << endl;
+
+	cout << fraction1 << " > " << fraction2 << " = " << (fraction1 > fraction2) << endl;
+	cout << fraction1 << " >= " << fraction2 << " = " << (fraction1 >= fraction2) << endl;
+	cout << fraction1 << " < " << fraction2 << " = " << (fraction1 < fraction2) << endl;
+	cout << fraction1 << " <= " << fraction2 << " = " << (fraction1 <= fraction2) << endl;
+	cout << fraction1 << " == " << fraction2 << " = " << (fraction1 == fraction2) << endl;
+	cout << fraction1 << " != " << fraction2 << " = " << (fraction1 != fraction2) << endl;
+}
+
